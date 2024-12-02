24957d512e98ae5c679ec7b5162f4fa41ce8c761
Aniket Singh <thor98571@gmail.com>
2024-12-02 16:34:23 +0530

    feat:added GA feature (#8)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/AppLayout/Header";
 import Footer from "@/components/AppLayout/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Procedure',
-    default: 'Home | Procedure', // a default is required when creating a template
+    template: "%s | Procedure",
+    default: "Home | Procedure", // a default is required when creating a template
   },
   description: "Think. Build. Measure.",
 };
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header />
         <main className="pt-12">{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-2KW21KL401" />
       </body>
     </html>
   );
